31b5b7c1b4021c8796ff1fb2819d016ac2c20cf7
Mert can Ünek <mrtcnunk@gmail.com>
2022-06-17 21:35:28 +0300

    First Commit

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour, IPooledObject
+{
+    public float UpForce = 1f;
+    public float SideForce = .1f;
+    
+    public void OnObjectSpawn()
+    {
+        float XForce = Random.Range(-SideForce, SideForce);
+        float YForce = Random.Range(UpForce / 2f, UpForce);
+        float ZForce = Random.Range(-SideForce, SideForce);
+
+
+        Vector3 force = new Vector3(XForce, YForce, ZForce);
+
+        GetComponent<Rigidbody>().velocity = force;
+    }
+
+}
